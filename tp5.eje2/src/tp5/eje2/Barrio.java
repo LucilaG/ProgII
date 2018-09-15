2f9958a595baa893a07e28f9d175cc3426ffa602
Gonzalo Erreca <gaudiolucila@gmail.com>
2018-09-15 14:23:03 -0300

    eliminar

--- tp5.eje2/src/tp5/eje2/Barrio.java
+++
@@ -1,5 +0,0 @@
-package tp5.eje2;
-
-public class Barrio {
-
-}
--- tp5.eje2/src/tp5/eje2/Lugar.java
+++
@@ -1,7 +0,0 @@
-package tp5.eje2;
-
-public abstract class Lugar {
-private String nombre;
-private int MAX = 3;
-private int[][] votos = new int[MAX][2];
-}
